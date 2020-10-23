4b562f0aed1def30afd279d76463d409fdf22bb2
Luan Mark <luan.mark13@gmail.com>
2020-10-23 16:36:51 -0300

    criando uma bib pro exercício

---
+++ Lista-2/Ex.30/funcs.h
@@ -0,0 +1,9 @@
+/**
+ * a. int ** criaMatriz(int m, int n)
+ * b. void leiaMatriz(int **mat, int m, int n)
+ * c. int somaMatriz(int **mat, int m, int n) }
+ * d. int* colunaMatriz(int ** mat, int m, int n, int ncoluna)
+ * e. void liberaMatriz(int **mat, int ncoluna)
+ * f. void imprimeMatriz(int **mat, int m, int n)
+ * g. void imprimeVetor (int *vet, int n) { }
+*/
