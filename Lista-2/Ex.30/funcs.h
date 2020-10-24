81279769a7c8c3aeb2f54f6115156fba5cc29557
Luan Mark <luan.mark13@gmail.com>
2020-10-23 22:41:54 -0300

    Exercício 30 finalizado

--- Lista-2/Ex.30/funcs.h
+++ Lista-2/Ex.30/funcs.h
@@ -1,9 +1,115 @@
-/**
- * a. int ** criaMatriz(int m, int n)
- * b. void leiaMatriz(int **mat, int m, int n)
- * c. int somaMatriz(int **mat, int m, int n) }
- * d. int* colunaMatriz(int ** mat, int m, int n, int ncoluna)
- * e. void liberaMatriz(int **mat, int ncoluna)
- * f. void imprimeMatriz(int **mat, int m, int n)
- * g. void imprimeVetor (int *vet, int n) { }
-*/
+
+int ** criaMatriz(int *tamanhoMatrizColunaLinha){
+    int **matriz;
+
+    // Limpa a tela
+    system("cls||clear");
+
+    printf("Digite quantas colunas quer que a matriz tenha: ");
+    scanf(" %d", &(tamanhoMatrizColunaLinha[0]));
+
+    printf("Digite quantas linhas quer que a matriz tenha: ");
+    scanf(" %d", &(tamanhoMatrizColunaLinha[1]));
+
+    //Aloca as colunas
+    matriz = (int **) malloc(sizeof(int *)*(tamanhoMatrizColunaLinha[0]));
+
+    //Aloca todas as linhas
+    for(int i = 0; i < tamanhoMatrizColunaLinha[0]; i++){
+        matriz[i] = (int *) malloc(sizeof(int) * tamanhoMatrizColunaLinha[1]);
+    }
+
+    system("cls||clear");
+
+    return matriz;
+}
+
+void inserirMatriz(int **matriz, int *tamanhoMatrizColunaLinha){
+
+    for(int coluna=0; coluna< tamanhoMatrizColunaLinha[0]; coluna++){
+        for (int  linha = 0; linha < tamanhoMatrizColunaLinha[1]; linha++)
+        {
+            printf("Digite o elemento %d,%d da matriz: ", coluna, linha);
+            scanf(" %d", &(matriz[coluna][linha]));
+        }
+        
+    }
+}
+
+int somaMatriz(int **matriz, int *tamanhoMatrizColunaLinha){
+    
+    int soma = 0;
+
+    for(int col = 0; col < tamanhoMatrizColunaLinha[0]; col++){
+        for (int lin = 0; lin < tamanhoMatrizColunaLinha[1]; lin++)
+        {
+            soma += matriz[col][lin];
+        }
+    }
+
+    printf("\n\nA soma dos elementos é %d\n", soma);
+
+}
+
+// Adiciona mais uma coluna na matriz
+int** inserirColuna(int ** matriz, int *tamanhoMatrizColunaLinha){
+
+    int escolha;
+
+    matriz = (int **)realloc(matriz, (tamanhoMatrizColunaLinha[0]+1)*sizeof(int*) );
+    matriz[tamanhoMatrizColunaLinha[0]] = (int *) malloc(sizeof(int) * tamanhoMatrizColunaLinha[1]);
+    
+    printf("Deseja preencher a nova coluna?\n");
+    printf("\t0 - Sim\n");
+    printf("\t1 - Não\n");
+    printf("Sua escolha: ");
+
+    scanf(" %d", &escolha);
+
+    if(!escolha){
+
+        for(int linha = 0; linha < tamanhoMatrizColunaLinha[1]; linha++){
+
+            printf("Digite o elemento %d,%d da matriz: ",
+                    tamanhoMatrizColunaLinha[0],
+                    linha
+            );
+            scanf(" %d", &(matriz[tamanhoMatrizColunaLinha[0]][linha]));
+
+        }
+
+    }
+    tamanhoMatrizColunaLinha[0] += 1;
+
+    return matriz;
+}
+
+void liberaMatriz(int **matriz, int *tamanhoMatrizColunaLinha){
+
+    for(int coluna=0; coluna< tamanhoMatrizColunaLinha[0]; coluna++){
+        free(matriz[coluna]);
+    }
+    free(matriz);
+}
+
+void imprimeMatriz(int **matriz, int *tamanhoMatrizColunaLinha){
+    
+    system("cls||clear");
+
+    for(int coluna=0; coluna< tamanhoMatrizColunaLinha[0]; coluna++){
+        for (int  linha = 0; linha < tamanhoMatrizColunaLinha[1]; linha++)
+        {
+            printf(" %d |", matriz[coluna][linha]);
+        }
+        printf("\n");
+    }
+}
+
+void imprimeVetor (int *vetor, int tamanhoVetor) {
+
+    for (int count = 0; count < tamanhoVetor; count++)
+    {
+        printf("%d\n", vetor[count]);
+    }
+    
+}
--- Lista-2/Ex.30/main.c
+++ Lista-2/Ex.30/main.c
@@ -1,10 +1,67 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include"funcs.h"
 
 int main(){
 
+    char input = '0';
+
+    //Salva as dimensões da matriz; pos 0 == coluna; pos 1 == linha
+    int tamanhoMatrizColunaLinha[2], 
+        **matriz;
+    
     
 
+    while (input != '6')
+    {
+        
+        // Menu
+        printf("\n\nEscolha sua opcao:\n");
+        printf("\t1 - Criar/redimensionar matriz\n");
+        printf("\t2 - Inserir elementos na matriz\n");
+        printf("\t3 - Exibir a soma dos elementos da matriz\n");
+        printf("\t4 - Inserir uma coluna nova\n");
+        printf("\t5 - Imprimir matriz\n");
+        printf("\t6 - Sair\n");
+        printf("Sua escolha: ");
+
+        scanf(" %c", &input);
+
+        switch (input)
+        {
+        case '1':
+            matriz = criaMatriz(tamanhoMatrizColunaLinha);
+        break;
+        
+        case '2':
+            inserirMatriz(matriz, tamanhoMatrizColunaLinha);
+        break;
+
+        case '3':
+            somaMatriz(matriz, tamanhoMatrizColunaLinha);
+        break;
+
+        case '4':
+            matriz = inserirColuna(matriz, tamanhoMatrizColunaLinha);
+        break;
+
+        case '5':
+            imprimeMatriz(matriz, tamanhoMatrizColunaLinha);
+        break;
+
+        case '6':
+            liberaMatriz(matriz, tamanhoMatrizColunaLinha);
+            return 0;
+        break;
+
+        default:
+        
+        break;
+        }
+
+    }
+    
+    
     return 0;
 }
